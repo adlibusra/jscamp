461a4fe829a71a2818aaf5276b21b500c8c73769
busra caliskan <busra.caliskan@overtech.com.tr>
2021-10-19 00:01:24 +0300

    first commit

---
+++ javaScriptStarterKit-master/src/app.js
@@ -0,0 +1,25 @@
+console.log("Merhab Kodlama.io")
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+dolarDun = "9"
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun= 11.2
+//euroDun=11 hata 
+
+console.log(euroDun)
+//camelcase : değişken
+//PascalCase
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi","Özel konutKredisi"]
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+ konutKredileri[i] + "</li>")
+    
+}
+console.log("</ul>")
